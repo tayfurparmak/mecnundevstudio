cf04d9a1b2fcb3686c14f4fe2dfb33d5080db99b
tayfur parmak <parmaktayfur@gmail.com>
2026-08-19 18:05:07 +0300

    prisma add

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import 'dotenv/config'
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
+    url: process.env.DIRECT_URL || process.env.DATABASE_URL!,
   },
 })
